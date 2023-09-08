7c0f71c643cae114f4a6e35c73343e8baad70878
khawla <11873365+BokahSan@users.noreplay.github.com>
2023-09-07 18:32:59 -0700

    hello 1.4.0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
+/**
+ * function main display "\"Programming is like building a multilingual puzzle"
+ * return 0 then exit the program
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
