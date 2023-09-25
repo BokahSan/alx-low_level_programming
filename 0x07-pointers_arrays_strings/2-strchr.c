e67ac2d3f9fc2888ef7928a954e11b7a79ef609e
khawla <11873365+BokahSan@users.noreplay.github.com>
2023-09-25 14:27:02 -0700

    qsldmo

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-	
-	while (*(s + i) != '\0')
+	while (*s != '\0')
 	{
-		if (*(s + i) == c)
-			return (s + i);
-		i++;
+		if (*s == c)
+			return ((char *)s);
+		s++;
 	}
-
-	return (NULL);
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
 }
